9fb2e76104374d4504707a9d81fb9ec8789b23b3
woanware <markwoan@outlook.com>
2021-03-18 08:26:17 +0000

    Added code

---
+++ src/CommandLine.cs
@@ -0,0 +1,17 @@
+﻿using CommandLine;
+
+namespace logsifter
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [Verb("sift", HelpText = "Stores meta data regarding disabled applications")]
+    class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Input file")]
+        public string Input { get; set; }
+         
+        [Option('d', "distance", Default = 0.30f, Required = false, HelpText = "Levenshtein distance value")]
+        public float Distance { get; set; }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,74 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace logsifter
+{
+    class Program
+    {
+        /// <summary>
+        /// Application entry point
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static int Main(string[] args)
+        {
+            return Parser.Default.ParseArguments<Options>(args)
+                .MapResult(
+                    (Options opts) => DoLogSift(opts),
+                    errs => 1);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="opts"></param>
+        static int DoLogSift(Options opts)
+        {
+            if (File.Exists(opts.Input) == false)
+            {
+                Console.WriteLine();
+                return 1;
+            }
+
+            using (StreamReader file = new StreamReader(opts.Input))
+            {
+                HashSet<string> data = new HashSet<string>();
+                string current;
+                bool display;
+                int length;
+                int distance;
+                float normalisedLev;
+
+                string previous = string.Empty;
+
+                while ((current = file.ReadLine()) != null)
+                {
+                    display = true;
+
+                    if (previous.Length > 0)
+                    {
+                        distance = Fastenshtein.Levenshtein.Distance(previous, current);
+                        length = Math.Max(previous.Length, current.Length);
+                        normalisedLev = (float)distance/ (float)length;
+
+                        if (normalisedLev <= opts.Distance)
+                        {
+                            display = false;
+                        }
+                    }
+
+                    if (display == true)
+                    {
+                        Console.WriteLine(current);
+                    }
+
+                    previous = current;
+                }
+            }               
+
+            return 0;
+        }
+    }
+}
